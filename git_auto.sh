47496534615a413df699ef5b6751410a5c2e0282
Chetan <ckalia@uwaterloo.ca>
2024-01-28 00:55:29 -0500

    WIP

--- git_auto.sh
+++ git_auto.sh
@@ -12,30 +12,32 @@ if [[ $PWD == '/' ]]; then
 fi
 
 # Check if there are any changes to commit
-# if [[ -n $(git status -s) ]]; then
+if [[ -n $(git status -s) ]]; then
 
-# Get the current branch name
-branch=$(git rev-parse --abbrev-ref HEAD)
+    # Get the current branch name
+    branch=$(git rev-parse --abbrev-ref HEAD)
 
-# Create upstream branch if doesn't exist
-git push -u origin $branch
+    # Create upstream branch if doesn't exist
+    git push -u origin $branch
 
-# Add all changes
-git add .
+    # Add all changes
+    git add .
 
-# Default commit message
-commit_message="WIP"
+    # Default commit message
+    commit_message="WIP"
 
-if [ "$1" ]; then
-    commit_message=$1        
+    if [ "$1" ]; then
+        commit_message=$1        
+    fi
+
+    # Commit with a default message or you can customize it
+    git commit -m "$commit_message" 
+
+    # Add remote repository if not already added
+    if [ "$2" ]; then
+        git remote add origin $2        
+    fi   
+
+    # Push to the remote repository
+    git push origin $branch
 fi
-
-# Commit with a default message or you can customize it
-git commit -m "$commit_message"   
-
-echo "commit"
-
-# Push to the remote repository
-# git push
-
-# fi
